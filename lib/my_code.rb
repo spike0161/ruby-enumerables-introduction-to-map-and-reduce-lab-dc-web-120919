7ad232d0f5b93bb3e64eed9dddca71196710eb00
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-09-07 19:27:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,8 +46,15 @@ def map_to_square(source_array)
 end
 
 
-def reduce_to_total(source_array, starting_point)
- return source_array.reduce
+def reduce_to_total(source_array, starting_point=0)
+ total = starting_point  
+  i = 0
+  
+while i < source_array.length do
+    total += source_array[i]
+    i += 1
+end
+total
 end
 
 def reduce_to_all_true(source_array)
