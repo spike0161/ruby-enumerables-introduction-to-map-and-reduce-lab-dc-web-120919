f3c34d9c0500b847d7b3d652e075924144295f7c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-09-07 18:16:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,14 @@
 # Map-like
 
 def map_to_negativize(source_array)
-  new_array = Array.new
-  i = 0
+  # new_array = Array.new
+  # i = 0
   
-    while i < source_array.length do
-      source_array[i] * -1
-    end
-  i += 1
-  return new_array
+  #   while i < source_array.length do
+  #     source_array[i] * -1
+  #   end
+  # i += 1
+  # return new_array
 end
 
 def map_to_no_change(source_array)
