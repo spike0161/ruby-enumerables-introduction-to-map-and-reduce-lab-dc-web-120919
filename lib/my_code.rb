c1ab7d9542cb00e5d0e4a40b8c198fe9953dff96
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-09-07 19:31:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,7 +58,13 @@ total
 end
 
 def reduce_to_all_true(source_array)
+  i = 0
   
+while i < source_array.length do
+    return false if source_array[i] == false
+    i += 1
+end
+total
 end
 
 def reduce_to_any_true(source_array)
