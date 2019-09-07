d47782293b39345dbc7781180cfbf25ff929d8b6
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-09-07 18:31:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,9 +2,9 @@
 
 def map_to_negativize(source_array)
  
- source_array.map { |n| n * -1}
+ return source_array.map { |n| n * -1}
  
-  return source_array
+  
 end
 
 def map_to_no_change(source_array)
