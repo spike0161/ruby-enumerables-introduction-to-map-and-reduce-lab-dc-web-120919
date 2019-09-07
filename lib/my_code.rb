e44643cf16180bb4f586fc8a55cbfc491dc26683
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-09-07 17:59:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# My Code here....
+# Map-like
+
+def map_to_negativize(source_array)
+  new_array = Array.new
+  i = 0
+  
+    while i < source_array.length do
+      
+    end
+  i += 1
+  
+end
+
+def map_to_no_change(source_array)
+  
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
+  
+end
